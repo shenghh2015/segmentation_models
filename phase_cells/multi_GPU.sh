91b1b6a3f70981586c6c317bd6311123ae5b3515
shenghh <shenghh2015@gmail.com>
2020-07-02 23:06:49 -0500

    v100

--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -4,7 +4,12 @@
 # JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --epoch 300 --lr 5e-4 --gpu 0
 # JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --epoch 300 --lr 5e-4 --gpu 0
 
-JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
-JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
-JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
-JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
+# JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
+# JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
+# JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
+# JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
+
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 0
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 1
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 2
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 3
