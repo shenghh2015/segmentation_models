713244bb5530711cc65b6b9aaeafc0826b09b423
Shenghua He <sh38@gauss.bioen.illinois.edu>
2020-07-02 22:27:26 -0500

    add phase_cells

---
+++ phase_cells/cell_cycle.py
@@ -0,0 +1,338 @@
+import os
+import cv2
+# import keras
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import segmentation_models as sm
+from segmentation_models import Unet, Linknet, PSPNet, FPN
+sm.set_framework('tf.keras')
+
+def str2bool(value):
+    return value.lower() == 'true'
+
+def generate_folder(folder_name):
+	if not os.path.exists(folder_name):
+		os.system('mkdir -p {}'.format(folder_name))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu", type=str, default = '0')
+parser.add_argument("--net_type", type=str, default = 'Unet')  #Unet, Linknet, PSPNet, FPN
+parser.add_argument("--backbone", type=str, default = 'resnet34')
+parser.add_argument("--down", type=str2bool, default = False)
+parser.add_argument("--epoch", type=int, default = 300)
+parser.add_argument("--batch_size", type=int, default = 2)
+parser.add_argument("--lr", type=float, default = 1e-3)
+parser.add_argument("--pre_train", type=str2bool, default = True)
+args = parser.parse_args()
+print(args)
+
+model_name = 'cellcycle-net-{}-bone-{}-pre-{}-epoch-{}-batch-{}-lr-{}-down-{}'.format(args.net_type, args.backbone, args.pre_train, args.epoch, args.batch_size, args.lr, args.down)
+print(model_name)
+
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+
+DATA_DIR = './data/cell_cycle2' if args.down else './data/cell_cycle'
+
+if args.down:
+	train_dim = 512; val_dim = 992
+else:
+	train_dim = 800; val_dim = 1942
+
+x_train_dir = os.path.join(DATA_DIR, 'train_images')
+y_train_dir = os.path.join(DATA_DIR, 'train_masks')
+
+x_valid_dir = os.path.join(DATA_DIR, 'val_images')
+y_valid_dir = os.path.join(DATA_DIR, 'val_masks')
+
+x_test_dir = os.path.join(DATA_DIR, 'test_images')
+y_test_dir = os.path.join(DATA_DIR, 'test_masks')
+
+# classes for data loading and preprocessing
+class Dataset:
+    """CamVid Dataset. Read images, apply augmentation and preprocessing transformations.
+    
+    Args:
+        images_dir (str): path to images folder
+        masks_dir (str): path to segmentation masks folder
+        class_values (list): values of classes to extract from segmentation mask
+        augmentation (albumentations.Compose): data transfromation pipeline 
+            (e.g. flip, scale, etc.)
+        preprocessing (albumentations.Compose): data preprocessing 
+            (e.g. noralization, shape manipulation, etc.)
+    
+    """
+    
+    CLASSES = ['bk', 'g1', 's', 'g2']
+    
+    def __init__(
+            self, 
+            images_dir, 
+            masks_dir, 
+            classes=None, 
+            augmentation=None, 
+            preprocessing=None,
+    ):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id) for image_id in self.ids]
+        
+        # convert str names to class values on masks
+        self.class_values = [self.CLASSES.index(cls.lower()) for cls in classes]
+        
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+    
+    def __getitem__(self, i):
+        
+        # read data
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks_fps[i], 0)
+#         print(np.unique(mask))
+        # extract certain classes from mask (e.g. cars)
+        masks = [(mask == v) for v in self.class_values]
+#         print(self.class_values)
+        mask = np.stack(masks, axis=-1).astype('float')
+        
+        # add background if mask is not binary
+        if mask.shape[-1] != 1:
+            background = 1 - mask.sum(axis=-1, keepdims=True)
+            mask = np.concatenate((mask, background), axis=-1)
+        
+        # apply augmentations
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # apply preprocessing
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+        
+    def __len__(self):
+        return len(self.ids)
+    
+    
+class Dataloder(tf.keras.utils.Sequence):
+    """Load data from dataset and form batches
+    
+    Args:
+        dataset: instance of Dataset class for image loading and preprocessing.
+        batch_size: Integet number of images in batch.
+        shuffle: Boolean, if `True` shuffle image indexes each epoch.
+    """
+    
+    def __init__(self, dataset, batch_size=1, shuffle=False):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.indexes = np.arange(len(dataset))
+
+        self.on_epoch_end()
+
+    def __getitem__(self, i):
+        
+        # collect batch data
+        start = i * self.batch_size
+        stop = (i + 1) * self.batch_size
+        data = []
+        for j in range(start, stop):
+            data.append(self.dataset[j])
+        
+        # transpose list of lists
+        batch = [np.stack(samples, axis=0) for samples in zip(*data)]
+        
+        return (batch[0], batch[1])
+    
+    def __len__(self):
+        """Denotes the number of batches per epoch"""
+        return len(self.indexes) // self.batch_size
+    
+    def on_epoch_end(self):
+        """Callback function to shuffle indexes each epoch"""
+        if self.shuffle:
+            self.indexes = np.random.permutation(self.indexes)
+
+import albumentations as A
+
+def round_clip_0_1(x, **kwargs):
+    return x.round().clip(0, 1)
+
+# define heavy augmentations
+def get_training_augmentation(dim):
+    train_transform = [
+
+        A.HorizontalFlip(p=0.5),
+
+        A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0),
+
+        A.PadIfNeeded(min_height=dim, min_width=dim, always_apply=True, border_mode=0),
+        A.RandomCrop(height=dim, width=dim, always_apply=True),
+
+        A.IAAAdditiveGaussianNoise(p=0.2),
+        A.IAAPerspective(p=0.5),
+
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightness(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.IAASharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+
+        A.OneOf(
+            [
+                A.RandomContrast(p=1),
+                A.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+        A.Lambda(mask=round_clip_0_1)
+    ]
+    return A.Compose(train_transform)
+
+
+def get_validation_augmentation(dim = 992):
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        A.PadIfNeeded(dim, dim)
+#         A.PadIfNeeded(384, 480)
+    ]
+    return A.Compose(test_transform)
+
+def get_preprocessing(preprocessing_fn):
+    """Construct preprocessing transform
+    
+    Args:
+        preprocessing_fn (callbale): data normalization function 
+            (can be specific for each pretrained neural network)
+    Return:
+        transform: albumentations.Compose
+    
+    """
+    
+    _transform = [
+        A.Lambda(image=preprocessing_fn),
+    ]
+    return A.Compose(_transform)
+
+
+# BACKBONE = 'efficientnetb3'
+BACKBONE = args.backbone
+BATCH_SIZE = args.batch_size
+CLASSES = ['g1', 's', 'g2']
+LR = args.lr
+EPOCHS = args.epoch
+
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+# define network parameters
+n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary and multiclass segmentation
+activation = 'sigmoid' if n_classes == 1 else 'softmax'
+
+#create model
+net_func = globals()[args.net_type]
+
+encoder_weights='imagenet' if args.pre_train else None
+
+model = net_func(BACKBONE, encoder_weights=encoder_weights, classes=n_classes, activation=activation)
+# model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+
+# define optomizer
+optim = tf.keras.optimizers.Adam(LR)
+
+# Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+# set class weights for dice_loss (car: 1.; pedestrian: 2.; background: 0.5;)
+dice_loss = sm.losses.DiceLoss(class_weights=np.array([1, 1, 1, 0.5]))
+focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.CategoricalFocalLoss()
+total_loss = dice_loss + (1 * focal_loss)
+
+# actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
+# total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss 
+
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+
+# compile keras model with defined optimozer, loss and metrics
+model.compile(optim, total_loss, metrics)
+
+# Dataset for train images
+train_dataset = Dataset(
+    x_train_dir, 
+    y_train_dir, 
+    classes=CLASSES, 
+    augmentation=get_training_augmentation(train_dim),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+# Dataset for validation images
+valid_dataset = Dataset(
+    x_valid_dir, 
+    y_valid_dir, 
+    classes=CLASSES, 
+    augmentation=get_validation_augmentation(val_dim),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+train_dataloader = Dataloder(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+valid_dataloader = Dataloder(valid_dataset, batch_size=1, shuffle=False)
+
+print(train_dataloader[0][0].shape)
+# check shapes for errors
+assert train_dataloader[0][0].shape == (BATCH_SIZE, train_dim, train_dim, 3)
+assert train_dataloader[0][1].shape == (BATCH_SIZE, train_dim, train_dim, n_classes)
+
+model_folder = './models/{}'.format(model_name)
+generate_folder(model_folder)
+
+
+# define callbacks for learning rate scheduling and best checkpoints saving
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(model_folder+'/best_model.h5', save_weights_only=True, save_best_only=True, mode='min'),
+    tf.keras.callbacks.ReduceLROnPlateau(),
+]
+
+# train model
+history = model.fit_generator(
+    train_dataloader, 
+    steps_per_epoch=len(train_dataloader), 
+    epochs=EPOCHS, 
+    callbacks=callbacks, 
+    validation_data=valid_dataloader, 
+    validation_steps=len(valid_dataloader),
+)
+
+# evaluate model
+test_dataset = Dataset(
+    x_test_dir, 
+    y_test_dir, 
+    classes=CLASSES, 
+    augmentation=get_validation_augmentation(val_dim),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+test_dataloader = Dataloder(test_dataset, batch_size=1, shuffle=False)
+# load best weights
+model.load_weights(model_folder+'/best_model.h5')
+scores = model.evaluate_generator(test_dataloader)
+print("Loss: {:.5}".format(scores[0]))
+for metric, value in zip(metrics, scores[1:]):
+    print("mean {}: {:.5}".format(metric.__name__, value))
+
+with open(model_folder+'/metric.txt','w+') as f:
+	for metric, value in zip(metrics, scores[1:]):
+		f.write("mean {}: {:.5}\n".format(metric.__name__, value))
