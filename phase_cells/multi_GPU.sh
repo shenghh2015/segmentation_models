6723ac738a4308737f78e61fcfd78987a11c9dc9
shenghh <shenghh2015@gmail.com>
2020-07-03 10:24:37 -0500

    v100

--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -34,7 +34,12 @@
 # JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb1 --pre_train True --batch_size 20 --dim 320 --epoch 600 --lr 5e-4 --gpu 2
 # JOB: python cell_cycle.py --net_type FPN --backbone efficientnetb0 --pre_train True --batch_size 20 --dim 320 --epoch 600 --lr 5e-4 --gpu 3
 
-JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 0
-JOB: python live_dead.py --net_type FPN --backbone efficientnetb2 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 1
-JOB: python live_dead.py --net_type FPN --backbone efficientnetb1 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 2
-JOB: python live_dead.py --net_type FPN --backbone efficientnetb0 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 3
+# JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 0
+# JOB: python live_dead.py --net_type FPN --backbone efficientnetb2 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 1
+# JOB: python live_dead.py --net_type FPN --backbone efficientnetb1 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 2
+# JOB: python live_dead.py --net_type FPN --backbone efficientnetb0 --pre_train True --batch_size 28 --dim 320 --epoch 600 --lr 5e-4 --gpu 3
+
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 20 --dim 320 --epoch 600 --lr 5e-4 --gpu 0
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb2 --pre_train True --batch_size 20 --dim 320 --epoch 600 --lr 5e-4 --gpu 1
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb1 --pre_train True --batch_size 20 --dim 320 --epoch 600 --lr 5e-4 --gpu 2
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb0 --pre_train True --batch_size 20 --dim 320 --epoch 600 --lr 5e-4 --gpu 3
