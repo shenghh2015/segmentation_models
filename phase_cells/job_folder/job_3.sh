61e2bb606bdf0d197b1afb7c045e166a8c33a5bb
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-07-02 23:24:46 -0500

    v100

--- phase_cells/job_folder/job_3.sh
+++ phase_cells/job_folder/job_3.sh
@@ -1,1 +1,1 @@
-python live_dead.py --net_type Unet --backbone efficientnetb6 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 3
+python cell_cycle.py --net_type Unet --backbone efficientnetb6 --pre_train True --batch_size 10 --epoch 300 --lr 5e-4 --gpu 3
