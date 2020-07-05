627728611d1dadd29962bab214a2b147f4542995
shenghh <shenghh2015@gmail.com>
2020-07-05 17:09:35 -0500

    v100

--- phase_cells/p0.sh
+++ phase_cells/p0.sh
@@ -1,4 +1,5 @@
 ## June 18
 # python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 8 --dim 320 --epoch 600 --lr 5e-4 --gpu 0
 
-python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 5 --dim 512 --epoch 200 --lr 5e-4 --train 900 --bk_weight 0.7 --gpu 0
+# python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 5 --dim 512 --epoch 200 --lr 5e-4 --train 900 --bk_weight 0.7 --gpu 0
+python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 5 --dim 512 --epoch 300 --lr 1e-4 --train 900 --gpu 0
--- phase_cells/p1.sh
+++ phase_cells/p1.sh
@@ -1,4 +1,5 @@
 # python cell_cycle.py --net_type FPN --backbone efficientnetb2 --pre_train True --batch_size 8 --dim 320 --epoch 600 --lr 5e-4 --gpu 1
 # python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 5 --dim 512 --train 100 --epoch 5 --lr 5e-4 --gpu 1
 
-python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 5 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.7 --gpu 1
+# python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 5 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.7 --gpu 1
+python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 512 --epoch 300 --lr 1e-4 --train 1100 --gpu 1
