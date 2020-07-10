2dfb398fbc0a5d01be38e762be2fa2c8ed4257fc
shenghh <shenghh2015@gmail.com>
2020-07-10 01:52:27 -0500

    v100

--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -137,7 +137,12 @@
 # JOB: python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --dim 512 --epoch 300 --lr 5e-4 --dataset live_dead_1664 --train 900 --rot 45 --gpu 2
 # JOB: python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 7 --dim 800 --epoch 300 --lr 5e-4 --dataset live_dead_1664 --train 900 --rot 45 --gpu 3
 
-JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 0
-JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 1
-JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb1 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 2
-JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 7 --dim 800 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 3
+# JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 0
+# JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 1
+# JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb1 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 2
+# JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 7 --dim 800 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 3
+
+JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --dim 512 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 45 --gpu 0
+JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 7 --dim 800 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 1
+JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb1 --pre_train True --batch_size 7 --dim 800 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 0 --gpu 2
+JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 7 --dim 800 --epoch 200 --lr 5e-4 --train 1100 --bk_weight 0.5 --dataset cell_cycle_1984 --rot 45 --gpu 3
