b97acd2d1b32adccad9da296fa32fc990f44efcf
shenghh <shenghh2015@gmail.com>
2020-07-11 12:30:16 -0500

    v100

--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -159,6 +159,6 @@
 
 ## July 11
 JOB: python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 200 --lr 5e-4 --dataset live_dead --down_factor 2 --train 900 --rot 0 --gpu 0
-JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 200 --lr 5e-4 --dataset live_dead --down_factor 2 --train 900 --rot 0 --gpu 1
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 20 --dim 320 --epoch 200 --lr 5e-4 --dataset live_dead --down_factor 2 --train 900 --rot 0 --gpu 1
 JOB: python live_dead.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 200 --lr 5e-4 --dataset live_dead --down_factor 2 --train 900 --rot 45 --gpu 0
-JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 200 --lr 5e-4 --dataset live_dead --down_factor 2 --train 900 --rot 45 --gpu 1
+JOB: python live_dead.py --net_type FPN --backbone efficientnetb3 --pre_train True --batch_size 20 --dim 320 --epoch 200 --lr 5e-4 --dataset live_dead --down_factor 2 --train 900 --rot 45 --gpu 1
