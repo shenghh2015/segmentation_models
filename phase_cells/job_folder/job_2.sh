f391bf010bfc310753abc9c9b2cdd6f226135ca3
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-07-25 02:10:06 -0500

    v100

--- phase_cells/job_folder/job_2.sh
+++ phase_cells/job_folder/job_2.sh
@@ -1,2 +1,2 @@
-python live_dead.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 512 --epoch 100 --lr 5e-4 --dataset live_dead --down_factor 1 --train 900 --class_balanced True --gpu 2 --loss focal+jaccard
+python live_dead.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 512 --epoch 100 --lr 5e-4 --dataset live_dead --down_factor 1 --train 900 --class_balanced True --gpu 2 --frozen True --loss focal+jaccard
 
--- phase_cells/job_folder/job_3.sh
+++ phase_cells/job_folder/job_3.sh
@@ -1,2 +1,1 @@
-python live_dead.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 512 --epoch 100 --lr 5e-4 --dataset live_dead --down_factor 1 --train 900 --class_balanced True --gpu 3 --loss focal+jaccard+dice
-
+python live_dead.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 512 --epoch 100 --lr 5e-4 --dataset live_dead --down_factor 1 --train 900 --class_balanced False --gpu 3 --frozen True --loss focal+jaccard
