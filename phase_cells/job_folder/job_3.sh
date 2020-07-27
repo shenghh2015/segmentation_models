53e78225a93959ca0e71e4513b771ed2a587b424
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-07-27 13:32:48 -0500

    v100

--- phase_cells/job_folder/job_3.sh
+++ phase_cells/job_folder/job_3.sh
@@ -1,1 +1,1 @@
-python life_cycle_flu.py --net_type Unet --backbone efficientnetb5 --pre_train True --batch_size 1 --dim 1024 --epoch 60 --lr 5e-4 --train 1100 --filtered True --gpu 3 --loss mse --act_fun linear
+python life_cycle_flu.py --net_type Unet --backbone efficientnetb5 --pre_train True --batch_size 1 --dim 1024 --epoch 60 --lr 5e-4 --train 1100 --filtered False --gpu 3 --loss mse --act_fun linear
