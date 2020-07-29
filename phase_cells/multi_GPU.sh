1d130dfb98456c79782b2574f9280abaad6ad2eb
shenghh <shenghh2015@gmail.com>
2020-07-29 09:09:28 -0500

    v100

--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -258,4 +258,4 @@
 JOB: python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --epoch 60 --lr 5e-4 --train 1100 --filtered True --gpu 0 --loss mse --act_fun relu  --channels combined --dataset cell_cycle_1984_v2
 JOB: python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --epoch 60 --lr 5e-4 --train 1100 --filtered True --gpu 1 --loss mse --act_fun linear --channels combined --dataset cell_cycle_1984_v2
 JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --down_factor 1 --epoch 150 --dataset cell_cycle_1984_v2 --lr 5e-4 --train 1100 --gpu 2 --loss focal+dice
-JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --down_factor 1 --epoch 150 --dataset cell_cycle_1984_v2 --lr 5e-4 --train 1100 --gpu 3 --loss focal+dice
+JOB: python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --down_factor 1 --epoch 150 --dataset cell_cycle_1984_v2 --lr 5e-4 --train 1100 --gpu 3 --loss focal+jaccard
