c44dd70f50e984ea078e5365bb14b01443d5efe5
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-07-29 14:44:43 -0500

    v100

--- phase_cells/job_folder/job_1.sh
+++ phase_cells/job_folder/job_1.sh
@@ -1,2 +1,2 @@
-python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 14 --dim 512 --epoch 150 --lr 5e-4 --train 1100 --filtered True --gpu 1 --loss mse --act_fun relu --channels combined --dataset cell_cycle_1984_v2
+python life_cycle_flu.py --net_type Unet --backbone efficientnetb1 --pre_train True --batch_size 6 --dim 800 --epoch 100 --lr 5e-4 --train 1100 --filtered True --gpu 1 --loss mae --act_fun relu --channels combined --dataset cell_cycle_1984_v2
 
--- phase_cells/job_folder/job_2.sh
+++ phase_cells/job_folder/job_2.sh
@@ -1,2 +1,2 @@
-python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 28 --dim 320 --epoch 200 --lr 5e-4 --train 1100 --filtered True --gpu 2 --loss mse --act_fun relu  --channels combined --dataset cell_cycle_1984_v2
+python life_cycle_flu.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 6 --dim 800 --epoch 100 --lr 5e-4 --train 1100 --filtered True --gpu 2 --loss mae --act_fun relu  --channels combined --dataset cell_cycle_1984_v2
 
