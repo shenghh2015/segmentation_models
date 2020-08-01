58828ca3c7e06a1ca7fe5345fc386604fd729d08
shenghh <shenghh2015@gmail.com>
2020-08-01 08:39:42 -0500

    v100

--- phase_cells/p1.sh
+++ phase_cells/p1.sh
@@ -7,4 +7,5 @@
 #python cell_cycle.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 320 --down_factor 2 --epoch 2 --dataset cell_cycle2 --lr 5e-4 --train 1100 --rot 0 --gpu 1
 
 # python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 1 --dim 1024 --epoch 60 --lr 5e-4 --train 1100 --filtered True --gpu 1 --loss mse --act_fun relu --channels combined --dataset cell_cycle_1984_v2
-python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 1 --dim 1024 --epoch 100 --lr 5e-4 --train 1100 --filtered False --gpu 1 --loss mse --act_fun sigmoid --channels combined --dataset cell_cycle_1984_v2 --ext True
+# python life_cycle_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 1 --dim 1024 --epoch 100 --lr 5e-4 --train 1100 --filtered False --gpu 1 --loss mse --act_fun sigmoid --channels combined --dataset cell_cycle_1984_v2 --ext True
+python phase_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 2 --dim 800 --epoch 60 --lr 5e-4 --train 1100 --filtered True --gpu 1 --loss mse --act_fun relu --channels fl2 --dataset cell_cycle_1984_v2 --ext False
