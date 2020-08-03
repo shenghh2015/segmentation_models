a2b12b492e59e264a224cce777f4dd92a249984e
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-08-03 10:06:49 -0500

    v100

--- phase_cells/job_folder/job_0.sh
+++ phase_cells/job_folder/job_0.sh
@@ -1,2 +1,2 @@
-python single_train.py --net_type FPN --backbone efficientnetb0 --pre_train True --batch_size 10 --dim 512 --epoch 120 --lr 5e-4 --dataset live_dead --train 900 --gpu 0 --loss focal+dice
+python single_train.py --net_type FPN --backbone efficientnetb0 --pre_train True --batch_size 4 --dim 800 --epoch 120 --lr 5e-4 --dataset cell_cycle_1984_v2 --train 900 --gpu 0 --loss focal+dice
 
