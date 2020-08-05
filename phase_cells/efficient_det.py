4d7ac6e442cac1517c606db76708fa748c89d032
shenghh <shenghh2015@gmail.com>
2020-08-05 03:49:39 -0500

    v100

--- phase_cells/efficient_det.py
+++ phase_cells/efficient_det.py
@@ -1,5 +1,5 @@
 import segmentation_models_v1 as sm
 import numpy as np
-model = sm.BiFPN('efficientnetb0', input_shape =(896,896,3), classes=4)
-y=model.predict(np.ones((1,1024,1024,3)))
+model = sm.BiFPN('efficientnetb0', input_shape =(512,512,3), classes=4)
+y=model.predict(np.ones((1,512,512,3)))
 y.shape
--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -75,7 +75,12 @@
 # JOB: python single_train.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 10 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 2 --loss focal+dice
 # JOB: python single_train.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 10 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 3 --loss focal+dice
 
-JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb0 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 0 --loss focal+dice
-JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb1 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 1 --loss focal+dice
-JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb2 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 2 --loss focal+dice
-JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb3 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 3 --loss focal+dice
+# JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb0 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 0 --loss focal+dice
+# JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb1 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 1 --loss focal+dice
+# JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb2 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 2 --loss focal+dice
+# JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb3 --pre_train True --batch_size 8 --dim 512 --epoch 120 --lr 1e-3 --dataset live_dead --train 900 --gpu 3 --loss focal+dice
+
+JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb0 --pre_train True --batch_size 4 --dim 800 --epoch 120 --lr 5e-4 --dataset cell_cycle_1984_v2 --train 900 --gpu 0 --loss focal+dice
+JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb1 --pre_train True --batch_size 4 --dim 800 --epoch 120 --lr 5e-4 --dataset cell_cycle_1984_v2 --train 900 --gpu 1 --loss focal+dice
+JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb2 --pre_train True --batch_size 4 --dim 800 --epoch 120 --lr 5e-4 --dataset cell_cycle_1984_v2 --train 900 --gpu 2 --loss focal+dice
+JOB: python BiFPN_train.py --net_type BiFPN --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --epoch 120 --lr 5e-4 --dataset cell_cycle_1984_v2 --train 900 --gpu 3 --loss focal+dice
