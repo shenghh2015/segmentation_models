b4b2f457c90a78c196a38d8397cf21503bcac358
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-08-07 23:35:50 -0500

    v100

--- phase_cells/job_folder/job_2.sh
+++ phase_cells/job_folder/job_2.sh
@@ -1,2 +1,2 @@
-python biFPN_train.py --net_type BiFPN --backbone efficientnetb2 --pre_train True --batch_size 3 --dim 896 --epoch 120 --lr 5e-4 --dataset cell_cycle_1984_v2 --train 900 --gpu 2 --loss focal+dice
+python single_train.py --net_type PSPNet --backbone efficientnetb2 --pre_train True --batch_size 14 --dim 528 --epoch 150 --lr 5e-4 --dataset live_dead --train 900 --gpu 2 --loss focal+dice
 
