547d00d9d8e82aa8fbef7210528dc7a29e2b3fb9
shenghh <shenghh2015@gmail.com>
2020-08-11 21:13:25 -0500

    v100

--- phase_cells/multi_GPU.sh
+++ phase_cells/multi_GPU.sh
@@ -110,7 +110,7 @@
 # JOB: python phase_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --epoch 80 --lr 5e-4 --train 1100 --filtered False --gpu 2 --loss mse+focal --act_fun sigmoid --channels fl1 --dataset cell_cycle_1984_v2 --ext False
 # JOB: python phase_flu.py --net_type Unet --backbone efficientnetb3 --pre_train True --batch_size 4 --dim 800 --epoch 80 --lr 5e-4 --train 1100 --filtered False --gpu 3 --loss mse+focal --act_fun sigmoid --channels fl2 --dataset cell_cycle_1984_v2 --ext False
 
-JOB: python single_train.py --net_type # Unet --backbone efficientnetb0 --pre_train True --batch_size 6 --dim 800 --epoch 150 --lr 5e-4 --dataset live_dead --train 900 --gpu 0 --loss focal+dice --filters 512 --upsample upsampling
+# JOB: python single_train.py --net_type Unet --backbone efficientnetb0 --pre_train True --batch_size 6 --dim 800 --epoch 150 --lr 5e-4 --dataset live_dead --train 900 --gpu 0 --loss focal+dice --filters 512 --upsample upsampling
 # JOB: python single_train.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 4 --dim 800 --epoch 150 --lr 5e-4 --dataset live_dead --train 900 --gpu 1 --loss focal+dice --filters 512 --upsample upsampling
 # JOB: python single_train.py --net_type Unet --backbone efficientnetb0 --pre_train True --batch_size 6 --dim 800 --epoch 150 --lr 5e-4 --dataset live_dead --train 900 --gpu 2 --loss focal+dice --filters 512 --upsample transpose
 # JOB: python single_train.py --net_type Unet --backbone efficientnetb2 --pre_train True --batch_size 4 --dim 800 --epoch 150 --lr 5e-4 --dataset live_dead --train 900 --gpu 3 --loss focal+dice --filters 512 --upsample transpose
