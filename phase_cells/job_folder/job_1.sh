3835c534c3e6ce7cff73f4e653238c1bcd4db855
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-09-14 22:31:03 -0500

    v100

--- phase_cells/job_folder/job_1.sh
+++ phase_cells/job_folder/job_1.sh
@@ -1,2 +1,2 @@
-python deeply_train.py --net_type DUNet --backbone efficientnetb1 --pre_train True --batch_size 10 --dim 512 --epoch 400 --lr 5e-4 --dataset live_dead --train 900 --gpu 1 --loss focal+dice --reduce_factor 1.0
+python deeply_train.py --net_type DUNet --backbone efficientnetb1 --pre_train True --batch_size 3 --dim 1024 --epoch 400 --lr 5e-4 --dataset cell_cycle_1984_v2 --ext True --train 1100 --gpu 1 --loss focal+dice --reduce_factor 1.0
 
