46f45aab00b1732f9e0355fc4ef84f253b3e5cfa
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-10-09 02:03:04 -0500

    v100

--- phase_cells/job_folder/job_1.sh
+++ phase_cells/job_folder/job_1.sh
@@ -1,2 +1,2 @@
-python single_train_v3.py --net_type Unet --backbone efficientnetb1 --pre_train True --batch_size 3 --dim 1024 --epoch 300 --lr 5e-4 --dataset cell_cycle_1984_v2 --ext True --train 1100 --gpu 1 --loss focal+dice --filters 256 --upsample upsampling --reduce_factor 0.8 --bk 1.0 --focal_weight 4
+python single_train_v4.py --net_type AtUnet --backbone efficientnetb1 --pre_train True --batch_size 3 --dim 1024 --epoch 300 --lr 5e-4 --dataset cell_cycle_1984_v2 --ext True --train 1100 --gpu 1 --loss focal+dice --filters 256 --upsample upsampling --reduce_factor 0.8 --bk 1.0 --focal_weight 4
 
