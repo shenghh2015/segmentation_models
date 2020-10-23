dd14a4fa7fe4035da0e63e11c96bfdc076015e9c
shenghh <shenghh2015@gmail.com>
2020-10-23 15:33:05 -0500

    v100

--- phase_fl/multi_GPU.sh
+++ phase_fl/multi_GPU.sh
@@ -4,7 +4,12 @@
 # JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb4 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 8 --rot 20 --lr 5e-4 --pre_train True --gpu 2
 # JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb5 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 6 --rot 20 --lr 5e-4 --pre_train True --gpu 3
 
-JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb6 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 0
-JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 1
+# JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb6 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 0
+# JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 1
+# JOB: python phase_fl_train.py --net_type AtUnet --backbone efficientnetb0 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 2
+# JOB: python phase_fl_train.py --net_type AtUnet --backbone efficientnetb1 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 3
+
+JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb6 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 0
+JOB: python phase_fl_train.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 1
 JOB: python phase_fl_train.py --net_type AtUnet --backbone efficientnetb0 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 2
 JOB: python phase_fl_train.py --net_type AtUnet --backbone efficientnetb1 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 3
