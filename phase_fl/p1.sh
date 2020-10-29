dfe186a983bc787d4bdf06848e68597cf0f07629
shenghh <shenghh2015@gmail.com>
2020-10-28 23:34:09 -0500

    v100

--- phase_fl/p1.sh
+++ phase_fl/p1.sh
@@ -1,3 +1,4 @@
 # python phase_fl_train.py --net_type Unet --backbone efficientnetb0 --dataset bead_dataset --epoch 200 --dim 512 --loss mae --batch_size 6 --lr 5e-4 --pre_train True --gpu 1
 # python phase_fl_train.py --net_type Unet --backbone efficientnetb1 --dataset neuron_x2 --epoch 200 --dim 512 --loss mse --batch_size 6 --rot 20 --lr 5e-4 --pre_train True --gpu 1
-python phase_fl_train3.py --net_type Unet --backbone efficientnetb0 --dataset neuron_x2 --epoch 1 --dim 512 --loss mse --batch_size 6 --rot 20 --lr 5e-4 --pre_train True --gpu 1 --ch_in 1 --ch_out 1
+python phase_fl_train3.py --net_type Unet --backbone efficientnetb1 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 6 --rot 20 --lr 5e-4 --pre_train True --gpu 1 --ch_in 3 --ch_out 1
+python phase_fl_train3.py --net_type Unet --backbone efficientnetb1 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 6 --rot 20 --lr 5e-4 --pre_train True --gpu 1 --ch_in 1 --ch_out 1
