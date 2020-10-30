2001b4e5c16a20595c11be8bb72e5a7d9248f357
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-10-30 00:21:08 -0500

    v100

--- phase_fl/job_folder/job_0.sh
+++ phase_fl/job_folder/job_0.sh
@@ -1,2 +1,2 @@
-python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 0 --ch_in 1 --ch_out 1
+python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 0 --ch_in 3 --ch_out 1
 
