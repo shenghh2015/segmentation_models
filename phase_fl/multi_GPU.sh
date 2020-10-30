3f6045c49c0f20a1bbac69c56d26181a83278d0c
shenghh <shenghh2015@gmail.com>
2020-10-30 00:05:04 -0500

    v100

--- phase_fl/multi_GPU.sh
+++ phase_fl/multi_GPU.sh
@@ -50,8 +50,12 @@
 # JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 2 --ch_in 3 --ch_out 1 --separate_mode 1
 # JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb0 --dataset neuron_x2 --epoch 100 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 3 --ch_in 3 --ch_out 1 --separate_mode 1
 
-JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 0 --ch_in 3 --ch_out 1
-JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 1 --ch_in 3 --ch_out 1 --separate_mode 1
-JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 2 --ch_in 3 --ch_out 1 --separate_mode 2
-JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 3 --ch_in 3 --ch_out 1 --scale 255
+# JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 0 --ch_in 3 --ch_out 1
+# JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 1 --ch_in 3 --ch_out 1 --separate_mode 1
+# JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 2 --ch_in 3 --ch_out 1 --separate_mode 2
+# JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb7 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 4 --rot 20 --lr 5e-4 --pre_train True --gpu 3 --ch_in 3 --ch_out 1 --scale 255
 
+JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb3 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 0 --ch_in 3 --ch_out 1
+JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb3 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 1 --ch_in 3 --ch_out 1 --separate_mode 1
+JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb3 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 2 --ch_in 3 --ch_out 1 --separate_mode 2
+JOB: python phase_fl_train3.py --net_type Unet --backbone efficientnetb3 --dataset neuron_x2 --subset train3_all --epoch 50 --dim 512 --loss mse --batch_size 14 --rot 20 --lr 5e-4 --pre_train True --gpu 3 --ch_in 3 --ch_out 1 --scale 255
