43de85814d288675aee306cf8fa5d9b90349f3ac
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-11-15 00:44:57 -0600

    v100

--- phase_fl/job_folder/job_1.sh
+++ phase_fl/job_folder/job_1.sh
@@ -1,2 +1,2 @@
-python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb7 --dataset spheroids_dataset_x1 --subset train --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl12 --ch_in 3 --ch_out 3
+python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_dataset_x1 --subset train --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl12 --ch_in 3 --ch_out 3
 
--- phase_fl/job_folder/job_3.sh
+++ phase_fl/job_folder/job_3.sh
@@ -1,1 +1,1 @@
-python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb7 --dataset spheroids_dataset_x1 --subset train --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-5 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3
+python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_dataset_x1 --subset train --epoch 100 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-5 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3
