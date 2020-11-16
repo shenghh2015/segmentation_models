072cb6b39644a1e645660a20342781dcea342829
shenghh <shenghh2015@gmail.com>
2020-11-16 14:11:06 -0600

    v100

--- phase_fl/infer_spheroids.sh
+++ phase_fl/infer_spheroids.sh
@@ -1,3 +1,4 @@
-python predict_spheroids.py --model_index 0 --gpu 2
-python predict_spheroids.py --model_index 1 --gpu 2
-python predict_spheroids.py --model_index 2 --gpu 2
+python predict_spheroids.py --model_index 0 --gpu 0
+python predict_spheroids.py --model_index 1 --gpu 0
+python predict_spheroids.py --model_index 2 --gpu 0
+python predict_spheroids.py --model_index 3 --gpu 0
