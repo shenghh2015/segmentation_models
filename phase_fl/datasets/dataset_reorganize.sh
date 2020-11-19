608c3fc4cf3051baa7ae690251fbd94c010f382d
Shenghua He <sh38@einstein.bioen.illinois.edu>
2020-11-18 19:46:50 -0600

    v100

---
+++ phase_fl/datasets/dataset_reorganize.sh
@@ -0,0 +1,30 @@
+mv neuron_trn_tst/test/phase/T-2-* neuron_trn_tst/train/phase
+mv neuron_trn_tst/test/phase/T-4-* neuron_trn_tst/train/phase
+mv neuron_trn_tst/test/phase/T-5-* neuron_trn_tst/train/phase
+mv neuron_trn_tst/test/phase/T-6-* neuron_trn_tst/train/phase
+
+mv neuron_trn_tst/test/fl1/T-2-* neuron_trn_tst/train/fl1
+mv neuron_trn_tst/test/fl1/T-4-* neuron_trn_tst/train/fl1
+mv neuron_trn_tst/test/fl1/T-5-* neuron_trn_tst/train/fl1
+mv neuron_trn_tst/test/fl1/T-6-* neuron_trn_tst/train/fl1
+
+mv neuron_trn_tst/test/fl2/T-2-* neuron_trn_tst/train/fl2
+mv neuron_trn_tst/test/fl2/T-4-* neuron_trn_tst/train/fl2
+mv neuron_trn_tst/test/fl2/T-5-* neuron_trn_tst/train/fl2
+mv neuron_trn_tst/test/fl2/T-6-* neuron_trn_tst/train/fl2
+
+mv neuron_trn_tst/train/phase/T-19-* neuron_trn_tst/test/phase
+mv neuron_trn_tst/train/phase/T-20-* neuron_trn_tst/test/phase
+mv neuron_trn_tst/train/phase/T-21-* neuron_trn_tst/test/phase
+mv neuron_trn_tst/train/phase/T-22-* neuron_trn_tst/test/phase
+
+mv neuron_trn_tst/train/fl1/T-19-* neuron_trn_tst/test/fl1
+mv neuron_trn_tst/train/fl1/T-20-* neuron_trn_tst/test/fl1
+mv neuron_trn_tst/train/fl1/T-21-* neuron_trn_tst/test/fl1
+mv neuron_trn_tst/train/fl1/T-22-* neuron_trn_tst/test/fl1
+
+mv neuron_trn_tst/train/fl2/T-19-* neuron_trn_tst/test/fl2
+mv neuron_trn_tst/train/fl2/T-20-* neuron_trn_tst/test/fl2
+mv neuron_trn_tst/train/fl2/T-21-* neuron_trn_tst/test/fl2
+mv neuron_trn_tst/train/fl2/T-22-* neuron_trn_tst/test/fl2
+
