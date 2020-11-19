1f01fd0feb26e6fdd597605e7acc8e807d0d67b7
shenghh <shenghh2015@gmail.com>
2020-11-18 19:58:22 -0600

    v100

--- phase_fl/local1.sh
+++ phase_fl/local1.sh
@@ -1,1 +1,1 @@
-python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 50 --dim 1760 --loss mse --batch_size 1 --rot 50 --lr 5e-5 --pre_train True --gpu 6 --fl_ch fl12 --ch_in 3 --ch_out 3 --docker False
+python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 50 --dim 1024 --loss mse --batch_size 2 --rot 50 --lr 5e-5 --pre_train True --gpu 6 --fl_ch fl12 --ch_in 3 --ch_out 3 --docker False
