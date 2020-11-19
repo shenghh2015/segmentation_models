7dd3ec2100568c01a80111cfb77e68350908333a
shenghh <shenghh2015@gmail.com>
2020-11-19 12:31:34 -0600

    v100

---
+++ phase_fl/test_neuron/run_inference_gauss2.sh
@@ -0,0 +1,4 @@
+python predict_neuron_gauss.py --gpu 0 --model_index 9 --save False
+python predict_neuron_gauss.py --gpu 0 --model_index 10 --save False
+python predict_neuron_gauss.py --gpu 0 --model_index 11 --save False
+python predict_neuron_gauss.py --gpu 0 --model_index 12 --save False
