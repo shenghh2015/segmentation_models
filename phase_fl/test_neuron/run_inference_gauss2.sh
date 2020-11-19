5789c88428fc8c3b6f659c5641f2c460cc80dd89
shenghh <shenghh2015@gmail.com>
2020-11-19 12:32:27 -0600

    v100

--- phase_fl/test_neuron/run_inference_gauss2.sh
+++ phase_fl/test_neuron/run_inference_gauss2.sh
@@ -1,4 +1,4 @@
-python predict_neuron_gauss.py --gpu 0 --model_index 9 --save False
-python predict_neuron_gauss.py --gpu 0 --model_index 10 --save False
-python predict_neuron_gauss.py --gpu 0 --model_index 11 --save False
-python predict_neuron_gauss.py --gpu 0 --model_index 12 --save False
+python predict_neuron_gauss.py --gpu 1 --model_index 9 --save False
+python predict_neuron_gauss.py --gpu 1 --model_index 10 --save False
+python predict_neuron_gauss.py --gpu 1 --model_index 11 --save False
+python predict_neuron_gauss.py --gpu 1 --model_index 12 --save False
