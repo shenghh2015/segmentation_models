10bf6b18c6399400a2feee988b76a579b197cf25
shenghh <shenghh2015@gmail.com>
2020-11-19 11:36:13 -0600

    v100

--- phase_fl/phase_fl1_fl2_train2.py
+++ phase_fl/phase_fl1_fl2_train2.py
@@ -214,41 +214,41 @@ def round_clip_0_1(x, **kwargs):
 def get_training_augmentation(dim, rot = 0):
     train_transform = [
 
-#         A.HorizontalFlip(p=0.5),
+        A.HorizontalFlip(p=0.5),
 # 
 #         A.ShiftScaleRotate(scale_limit=0.5, rotate_limit=rot, shift_limit=0.1, p=1, border_mode=0),
 # 
         A.PadIfNeeded(min_height=dim, min_width=dim, always_apply=True, border_mode=0),
         A.RandomCrop(height=dim, width=dim, always_apply=True),
 # 
-        A.IAAAdditiveGaussianNoise(p=0.2),
-        A.IAAPerspective(p=0.5),
-
-        A.OneOf(
-            [
-                A.CLAHE(p=1),
-                A.RandomBrightness(p=1),
-                A.RandomGamma(p=1),
-            ],
-            p=0.9,
-        ),
-
-        A.OneOf(
-            [
-                A.IAASharpen(p=1),
-                A.Blur(blur_limit=3, p=1),
-                A.MotionBlur(blur_limit=3, p=1),
-            ],
-            p=0.9,
-        ),
-
-        A.OneOf(
-            [
-                A.RandomContrast(p=1),
-                A.HueSaturationValue(p=1),
-            ],
-            p=0.9,
-        ),
+#         A.IAAAdditiveGaussianNoise(p=0.2),
+#         A.IAAPerspective(p=0.5),
+# 
+#         A.OneOf(
+#             [
+#                 A.CLAHE(p=1),
+#                 A.RandomBrightness(p=1),
+#                 A.RandomGamma(p=1),
+#             ],
+#             p=0.9,
+#         ),
+# 
+#         A.OneOf(
+#             [
+#                 A.IAASharpen(p=1),
+#                 A.Blur(blur_limit=3, p=1),
+#                 A.MotionBlur(blur_limit=3, p=1),
+#             ],
+#             p=0.9,
+#         ),
+# 
+#         A.OneOf(
+#             [
+#                 A.RandomContrast(p=1),
+#                 A.HueSaturationValue(p=1),
+#             ],
+#             p=0.9,
+#         ),
 #         A.Lambda(mask=round_clip_0_1)
     ]
     return A.Compose(train_transform)
