ecb730456d43a03ac5fb845d199faf3c5d6c8d33
shenghh <shenghh2015@gmail.com>
2020-11-21 17:13:07 -0600

    v100

--- phase_fl/multi_GPU.sh
+++ phase_fl/multi_GPU.sh
@@ -123,6 +123,6 @@
 # JOB: python phase_fl1_fl2_train2.py --net_type Unet --backbone efficientnetb0 --dataset spheroids_dataset_x1 --subset train --epoch 150 --dim 512 --loss mse --batch_size 14 --rot 50 --lr 1e-4 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best True
 
 JOB: python train_model.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 200 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 5e-4 --pre_train True --gpu 0,1,2,3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best False
-# JOB: python train_model.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 150 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl12 --ch_in 3 --ch_out 3 --best False
+JOB: python train_model.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 150 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl12 --ch_in 3 --ch_out 3 --best False
 # JOB: python train_model.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 150 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 5e-5 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best False
 # JOB: python train_model.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst --subset train --epoch 150 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-5 --pre_train True --gpu 4 --fl_ch fl12 --ch_in 3 --ch_out 3 --best False
