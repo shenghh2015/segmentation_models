cab63409b30a049eda96577286c44469b5028be0
shenghh <shenghh2015@gmail.com>
2020-11-23 01:43:31 -0600

    v100

--- phase_fl/local.sh
+++ phase_fl/local.sh
@@ -19,4 +19,5 @@ python train_model.py --net_type Unet --backbone efficientnetb3 --dataset sphero
 python train_model.py --net_type Unet --backbone efficientnetb3 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 5e-4 --pre_train True --gpu 7 --fl_ch fl2 --ch_in 3 --ch_out 3 --docker False
 
 # turing
- 
+python train_neuron.py --net_type Unet --backbone efficientnetb3 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 5e-4 --pre_train True --gpu 0 --fl_ch fl1 --ch_in 3 --ch_out 3 --docker False
+python train_neuron.py --net_type Unet --backbone efficientnetb3 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 5e-4 --pre_train True --gpu 7 --fl_ch fl2 --ch_in 3 --ch_out 3 --docker False
