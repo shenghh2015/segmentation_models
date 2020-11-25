b0dff98cb16a94d03752c959cf7e4d162eacdcbf
shenghh <shenghh2015@gmail.com>
2020-11-25 17:35:20 -0600

    v100

--- phase_fl/multi_GPU.sh
+++ phase_fl/multi_GPU.sh
@@ -148,14 +148,15 @@
 # JOB: python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-5 --pre_train True --gpu 3 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True
 
 ## Nov. 25, 2020
-JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True
-JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --extra False
-JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 2 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True
+# JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True
+# JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --extra False
+# JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 2 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True
+
+JOB: python train_model.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True
+JOB: python train_model.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True
+JOB: python train_model.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 2 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True
 JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True --extra False
 
-# JOB: python train_model.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True
-# JOB: python train_model.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 1 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True
-# JOB: python train_model.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v3 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 2 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True
 # JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 3 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True --extra False
 
 # JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True
