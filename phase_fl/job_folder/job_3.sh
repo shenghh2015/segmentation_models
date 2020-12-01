27aa929e3353f1ff1b6da0363d85f01e79fe3ab0
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-12-01 01:40:46 -0600

    v100

--- phase_fl/job_folder/job_3.sh
+++ phase_fl/job_folder/job_3.sh
@@ -1,1 +1,1 @@
-python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True --scale 10.0 --extra False
+python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True --scale 1.0 --extra False
