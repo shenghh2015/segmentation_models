0fa4759729716ea2a00a8d52073ae7b9ee86371f
shenghh <shenghh2015@gmail.com>
2020-12-04 17:17:58 -0600

    v100

--- phase_fl/multi_GPU.sh
+++ phase_fl/multi_GPU.sh
@@ -190,4 +190,10 @@
 # JOB: python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 2 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --scale 1.0 --extra True
 # JOB: python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 3 --fl_ch fl12 --ch_in 3 --ch_out 3 --best_select True --scale 1.0 --extra True
 
-JOB: python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --scale 10.0 --extra True
+# JOB: python train_neuron.py --net_type Unet --backbone efficientnetb7 --dataset neuron_trn_tst_v2 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 0 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --scale 10.0 --extra True
+
+##
+JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v4 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 4 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True --scale 100.
+JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v4 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 4 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --scale 100.
+JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v5 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 4 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True --scale 100.
+JOB: python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v5 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 4 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --scale 100.
