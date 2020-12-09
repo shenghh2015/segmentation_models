6c5280a9876cf2f1677b8d866d8f14a04590e431
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2020-12-08 18:05:48 -0600

    v100

--- phase_fl/job_folder/job_0.sh
+++
@@ -1,2 +0,0 @@
-python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v4 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 4 --fl_ch fl1 --ch_in 3 --ch_out 3 --best_select True --scale 100.
-
--- phase_fl/job_folder/job_3.sh
+++
@@ -1,1 +0,0 @@
-python train_neuron.py --net_type Unet --backbone efficientnetb6 --dataset spheroids_v5 --subset train --epoch 800 --dim 512 --loss mse --batch_size 4 --rot 50 --lr 1e-4 --pre_train True --gpu 4 --fl_ch fl2 --ch_in 3 --ch_out 3 --best_select True --scale 100.
