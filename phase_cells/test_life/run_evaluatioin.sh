2a6c47efa33bae22cf10275a7c66c1c8dd842e05
shenghh <shenghh2015@gmail.com>
2020-12-19 20:23:03 -0600

    v100

--- phase_cells/test_life/run_evaluatioin.sh
+++ phase_cells/test_life/run_evaluatioin.sh
@@ -1,2 +1,6 @@
 python evaluate_life_cycle.py --model_index 0
 python evaluate_life_cycle.py --model_index 1
+python evaluate_life_cycle.py --model_index 2
+python evaluate_life_cycle.py --model_index 3
+python evaluate_life_cycle.py --model_index 4
+python evaluate_life_cycle.py --model_index 5
