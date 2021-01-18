296964080186725bd38c91199ab22e2f83eba751
shenghh <shenghh2015@gmail.com>
2021-01-18 09:43:24 -0600

    v100

--- cell_cycle/test_viability/run_evaluatioin.sh
+++ cell_cycle/test_viability/run_evaluatioin.sh
@@ -4,11 +4,10 @@
 # python evaluate_via.py --gpu 2 --model_index 3
 # python evaluate_via.py --gpu 2 --model_index 4
 # python evaluate_via.py --gpu 2 --model_index 5
-# python evaluate_via.py --gpu 2 --model_index 6
-# python evaluate_via.py --gpu 2 --model_index 7
-# python evaluate_via.py --gpu 2 --model_index 8
-# python evaluate_via.py --gpu 2 --model_index 9
-# python evaluate_via.py --gpu 2 --model_index 8
-python evaluate_via.py --gpu 2 --model_index 10
-python evaluate_via.py --gpu 2 --model_index 11
-python evaluate_via.py --gpu 2 --model_index 12
+python evaluate_via.py --gpu 2 --model_index 6
+python evaluate_via.py --gpu 2 --model_index 7
+python evaluate_via.py --gpu 2 --model_index 8
+python evaluate_via.py --gpu 2 --model_index 9
+# python evaluate_via.py --gpu 2 --model_index 10
+# python evaluate_via.py --gpu 2 --model_index 11
+# python evaluate_via.py --gpu 2 --model_index 12
