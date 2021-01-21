6d562bc1aa029588f3ebacdbfc31852126360ec8
shenghh <shenghh2015@gmail.com>
2021-01-20 20:10:45 -0600

    v100

--- cell_cycle/test_viability/run_via2.sh
+++ cell_cycle/test_viability/run_via2.sh
@@ -1,2 +1,4 @@
 # python eval_via2.py --gpu 2 --model_file model_via2.txt --model_index 0
-python eval_via2.py --gpu 2 --model_file model_via2.txt --model_index 1
+# python eval_via2.py --gpu 2 --model_file model_via2.txt --model_index 1
+python eval_via2.py --gpu 2 --model_file model_via2.txt --model_index 2
+python eval_via2.py --gpu 2 --model_file model_via2.txt --model_index 3
