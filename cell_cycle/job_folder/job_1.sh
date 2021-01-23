1abcae5e411a8bb95594fcd42dd9d6c0f615117a
shenghh2015@gmail.com <shenghuahe@compute1-client-1.ris.wustl.edu>
2021-01-23 10:49:23 -0600

    v100

--- cell_cycle/job_folder/job_1.sh
+++ cell_cycle/job_folder/job_1.sh
@@ -1,2 +1,2 @@
-python train_viability.py --net_type Unet --backbone efficientnetb5 --pre_train True --batch_size 4 --dim 800 --epoch 2400 --lr 1e-4 --dataset viability2_832x832 --data_version 3 --gpu 1 --loss focal+dice --filters 256 --upsample upsampling --reduce_factor 1.0 --bk 1.0 --focal_weight 4 
+python train_model.py --net_type Unet --backbone efficientnetb5 --pre_train True --batch_size 4 --dim 800 --epoch 4800 --lr 1e-4 --dataset cyc2_1488x1512 --gpu 1 --loss focal+dice --filters 256 --upsample upsampling --reduce_factor 1.0 --bk 1.0 --focal_weight 4 
 
